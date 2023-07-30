fded04ab0040bae74ede604904f7fbb0d7ca7c56
Noor <nooralhudaatiq@gmail.com>
2023-07-30 11:25:43 +0430

    Assignment #0.11

---
+++ Assignment/Assignment11th/ifstatment.py
@@ -0,0 +1,13 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("The weather is hot")
+    print("Drink cold drinks")
+elif is_cold:
+     print("The weather is cold")
+     print("Wear warm clothes")
+else:
+    print("The waether is Lovely")
+
+print("Enjoy your day")
---
+++ Assignment/Assignment11th/lenght.py
@@ -0,0 +1,8 @@
+name = "Noorulhuda Atiq"
+
+if len(name) < 3:
+    print("Name must be at least 3 character")
+elif len(name) > 50:
+    print("Name must be a maxmimum of 50 characters")
+else:
+    print("Name looks good!")
